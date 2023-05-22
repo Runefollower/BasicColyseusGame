d47ea1407347e1df773347c1a0d6e306b682aa5a
Jim McGuire <jim@mcmar.com>
2023-05-21 21:16:31 -0400

    Added detailed comments

--- src/client/GameEntities.ts
+++ src/client/GameEntities.ts
@@ -1,18 +1,22 @@
+/**
+ * GameObject is an abstract class that provides the foundation for any object in the game.
+ * This includes basic attributes like x and y coordinates, velocity, direction, and acceleration.
+ * It also includes an update function to handle movement and a draw function to be implemented by child classes.
+ */
 abstract class GameObject {
-  x: number;
-  y: number;
+  x: number; // x-coordinate of the object
+  y: number; // y-coordinate of the object
+  rx: number; // x-coordinate for rendering (interpolated from the last server update)
+  ry: number; // y-coordinate for rendering (interpolated from the last server update)
+  vx: number; // x-velocity of the object
+  vy: number; // y-velocity of the object
+  direction: number; // direction the object is facing
+  vr: number; // rate of change of direction
+  accel: number; // object's acceleration
 
-  //Render x and Render y - the positions
-  //the object is displayed at based on interpolating 
-  //from last server update
-  rx: number;
-  ry: number;
-  vx: number;
-  vy: number;
-  direction: number;
-  vr: number;
-  accel: number;
-
+  /**
+   * Constructs a new GameObject.
+   */
   constructor(x: number, y: number, vx: number, vy: number, direction: number, vr: number, accel: number) {
     this.x = x;
     this.y = y;
@@ -23,25 +27,34 @@ abstract class GameObject {
     this.accel = accel;
   }
 
-  // Update this entity. UDT is the time since last 
-  //server update.  This is just taking care of any movement
-  //since the last server call
+  /**
+   * Updates the GameObject's position based on its velocity and the time since the last server update.
+   */
   update(udt: number): void {
     this.rx = this.x + this.vx * udt;
     this.ry = this.y + this.vy * udt;
   }
 
+  /**
+   * Abstract method for drawing the object. This needs to be implemented by any child classes.
+   */
   abstract draw(ctx: CanvasRenderingContext2D): void;
 }
 
-
+/**
+ * PlayerShip extends GameObject and represents a player's ship in the game.
+ * It includes additional attributes specific to the player's ship, like name, firing state, last fired time, score, and session ID.
+ */
 export class PlayerShip extends GameObject {
-  name: string;
-  firing: boolean;
-  lastFired: number;
-  score: number;
-  sessionId: string;
+  name: string; // player's name
+  firing: boolean; // if the ship is currently firing
+  lastFired: number; // the time the ship last fired
+  score: number; // player's score
+  sessionId: string; // unique session ID for the player
 
+  /**
+   * Constructs a new PlayerShip.
+   */
   constructor(x: number, y: number, vx: number, vy: number, direction: number, vr: number, accel: number, name: string, firing: boolean, lastFired: number, score: number, sessionId: string) {
     super(x, y, vx, vy, direction, vr, accel);
     this.name = name;
@@ -51,15 +64,24 @@ export class PlayerShip extends GameObject {
     this.sessionId = sessionId;
   }
 
+  /**
+   * Implementation of the abstract draw method for PlayerShip. 
+   * This method would contain code to draw the player's ship on the canvas.
+   */
   draw(ctx: CanvasRenderingContext2D): void {
   }
 }
 
-
-
-
+/**
+ * Laser extends GameObject and represents a laser in the game.
+ * Currently, it has no additional properties or methods beyond those provided by GameObject.
+ */
 export class Laser extends GameObject {
 
+  /**
+   * Implementation of the abstract draw method for Laser.
+   * This method would contain code to draw a laser on the canvas.
+   */
   draw(ctx: CanvasRenderingContext2D): void {
   }
 }
